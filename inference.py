92ef1b8c9cfc6ffadbdd07fa0cb4094f72cf3fe6
ParthMahajan21 <parthmahajan363@gmail.com>
2026-04-08 00:17:46 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -4,19 +4,14 @@ from env import RetailEnv
 app = FastAPI()
 env = RetailEnv()
 
-# ✅ RESET
 @app.post("/reset")
 def reset():
     state = env.reset()
-    return {
-        "state": state
-    }
+    return {"state": state}
 
-# ✅ STEP
 @app.post("/step")
 def step(action: dict):
     state, reward, done, info = env.step(action)
-
     return {
         "state": state,
         "reward": reward,
